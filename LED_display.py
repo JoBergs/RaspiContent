01841cdb3aec573ea1fb7cddf5ec35d56e3d4601
Johannes Bergs <jo@knight-of-pi.org>
2015-11-02 14:32:49 +0100

    adding display logic

--- LED_display.py
+++ LED_display.py
@@ -9,6 +9,22 @@ rows = [10, 11, 12]  # 16, 18, 40 on board
 columns = [19, 21, 23]  # 16, 18, 40 on board
 GPIO.setup(rows + columns, GPIO.OUT)
 
+# it may be better to make functions which generate lists of tuples of pins to activate
+# for decimal:
+#   take the input number modulo 10
+#   i = 0
+#   for col in cols
+#       GPIO.output(columns[col], GPIO.HIGH)
+#       for row in rows
+#           if i < input
+#               GPIO.output(columns[col], GPIO.HIGH)
+#           else
+#               break;
+#       i += 1
+#           
+
+# make function all_off 
+
 if __name__ == '__main__':
     GPIO.output(columns[0], GPIO.HIGH)
     GPIO.output(rows[0], GPIO.HIGH)
