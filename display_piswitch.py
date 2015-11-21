3fe78938f95d5a89ab2ae3378065f2254751c75b
Johannes Bergs <jo@knight-of-pi.org>
2015-11-21 16:44:53 +0100

    cleanup display_piswitch

--- display_piswitch.py
+++ display_piswitch.py
@@ -24,60 +24,6 @@ from pi_switch import RCSwitchReceiver
 
 from LED_display import display_number
 
-
-'''
-GPIO.setmode(GPIO.BOARD)
-pins = [10, 11, 15, 16, 32, 33, 35, 36]
-GPIO.setup(pins, GPIO.OUT)
-
-
-def convert_to_decimal(number):
-    return pins[:min(number,9)]
-
-def convert_to_binary(number):
-    binary = bin(number)[-2::-1]
-    print(binary)
-    return [pins[i] for i in range(len(binary)) if binary[i] == '1']
-
-
-def display_pins(pin_list, duration):
-    GPIO.output(pin_list, GPIO.HIGH)
-    if duration:
-        sleep(duration)
-        all_off()
-
-def all_off():
-    GPIO.output(pins, GPIO.LOW)  
-
-# permanent should be delay interval
-# if duration is False, the lights stay on until a new signal arrives
-#   if duration is an float > 0, the leds will be lighted for that number of seconds
-def display_number(number=5, as_binary=False, duration=False):
-    all_off()
-    print(number, as_binary, duration)
-
-    pin_list = convert_to_decimal(number)
-
-    if as_binary:
-        pin_list = convert_to_binary(number)
-
-    display_pins(pin_list, duration)
-'''
-
-
-# make parameter 'permanent' which decides wheter all_off is executed after displaying
-
-# could this be made a decorator? how do i use it in RL?
-
-# a decorator which displays the return value?
-
-
-# needs to be importable
-
-# rethink: binary should be a flag
-
-# keyboard interrupt -> GPIO.cleanup()
-
 # fuse sender and receiver into one class?
 
 # This is the sender script:
