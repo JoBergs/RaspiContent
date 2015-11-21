ecb58d828395dd3cdf72d8893e9f2c497d4ee1d8
Johannes Bergs <jo@knight-of-pi.org>
2015-11-21 17:55:53 +0100

    display_piswitch cleanup

--- display_piswitch.py
+++ display_piswitch.py
@@ -17,44 +17,38 @@ def run_sender():
     sender = pi_switch.RCSwitchSender()
     sender.enableTransmit(0) # use WiringPi pin 0
     num = 1
+
     while True:
-        print 'sending'
+        print 'Sending: ' + str(num)
         sleep(1)
         sender.sendDecimal(num, 24)
-        num += 1
-        print(num)
+        num += 1   
 
 def run_receiver():
+    receiver = RCSwitchReceiver()
+    receiver.enableReceive(2)
+
     while True:
-        display_number(randint(0, 100), True, 0.2)
         print 'Listening...'
         sleep(1)
+
         if receiver.available():
             received_value = receiver.getReceivedValue()
+            
             if received_value:
-                num += 1
-                #print("Received[%s]:" % num)
-                # needs a % 256
                 display_number(received_value % 256, True)
-                # cleanup below
+
                 print("Value: %s | Bit: %s | Protocol: %s" % (received_value,
                         receiver.getReceivedBitlength(),
                         receiver.getReceivedProtocol()))
-                #print("Protocol: %s\n" % receiver.getReceivedProtocol())
 
             receiver.resetAvailable()  
 
 
 if __name__ == '__main__':
 
-    receiver = RCSwitchReceiver()
-    receiver.enableReceive(2)
-
-    num = 0
-
     try:
-        # HACKish: anything besides "sender" being the last parameter
-        #                creates a receiver
+        # HACKish: "sender" as last parameter - be a sender; be a receiver else
         if sys.argv[-1] == "sender":
             run_sender()
         else:
