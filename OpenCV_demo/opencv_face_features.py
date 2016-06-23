c42a1cbbb47e4fb5b25fada45994a7b152e3ad7a
JoBergs <jo@knight-of-pi.org>
2016-06-23 13:48:08 +0200

    functions for opencv_face_features

--- OpenCV_demo/opencv_face_features.py
+++ OpenCV_demo/opencv_face_features.py
@@ -9,35 +9,40 @@ import cv2
 #   detect eyes, ears, mouth and nose (maybe with labels?)
 #   if no parameter is given, a snapshot shall be made
 
-face_cascade = cv2.CascadeClassifier('/home/pi/opencv-3.0.0/data/haarcascades/haarcascade_frontalface_default.xml')
-#eye_cascade = cv2.CascadeClassifier('/home/pi/opencv-3.0.0/data/haarcascades/haarcascade_eye.xml')
+def extract_features(image):
+    face_cascade = cv2.CascadeClassifier('/home/pi/opencv-3.0.0/data/haarcascades/haarcascade_frontalface_default.xml')
+    #eye_cascade = cv2.CascadeClassifier('/home/pi/opencv-3.0.0/data/haarcascades/haarcascade_eye.xml')
 
-image = cv2.imread('poi_1.jpg')
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    image = cv2.imread('poi_1.jpg')
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-faces = face_cascade.detectMultiScale(
-    gray,
-    scaleFactor=1.1,
-    minNeighbors=5,
-    minSize=(30, 30),
-    flags = cv2.CASCADE_SCALE_IMAGE
-)
+    faces = face_cascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30),
+        flags = cv2.CASCADE_SCALE_IMAGE
+    )
 
-print "Found {0} faces!".format(len(faces))
+    print "Found {0} faces!".format(len(faces))
 
-# Draw a rectangle around the faces
-for (x, y, w, h) in faces:
-    cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    # Draw a rectangle around the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
 
-cv2.imshow('img', image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    cv2.imshow('img', image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+def make_snapshot():
+    image = cv2.imread('poi_1.jpg')
+    return image
 
 if __name__ == "__main__":
     if len(sys.argv) >= 2:
-        foto=Image(sys.argv[1])
+        image = cv2.imread(sys.argv[1])
     else:
-        foto = features_from_snapshot()
+        image = make_snapshot()
 
-    extract_features(foto)
+    extract_features(image)
