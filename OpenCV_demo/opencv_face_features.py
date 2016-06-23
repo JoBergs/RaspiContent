38461d07b5402d869f574c31a378391cd8f24495
JoBergs <jo@knight-of-pi.org>
2016-06-23 15:33:17 +0200

    finalized opencv demo script

--- OpenCV_demo/opencv_face_features.py
+++ OpenCV_demo/opencv_face_features.py
@@ -1,19 +1,15 @@
 #!/usr/bin/python2.7
 
-import numpy as np
+import sys
+from time import sleep
 import cv2
-
-# howto set the system path right?
-
-# todo:
-#   detect eyes, ears, mouth and nose (maybe with labels?)
-#   if no parameter is given, a snapshot shall be made
+import picamera
 
 def extract_features(image):
     face_cascade = cv2.CascadeClassifier('/home/pi/opencv-3.0.0/data/haarcascades/haarcascade_frontalface_default.xml')
-    #eye_cascade = cv2.CascadeClassifier('/home/pi/opencv-3.0.0/data/haarcascades/haarcascade_eye.xml')
-
-    image = cv2.imread('poi_1.jpg')
+    eye_cascade = cv2.CascadeClassifier('/home/pi/opencv-3.0.0/data/haarcascades/haarcascade_eye.xml')
+    #nose_cascade = cv2.CascadeClassifier('/home/pi/opencv-3.0.0/data/haarcascades/Nariz.xml')
+    
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
     faces = face_cascade.detectMultiScale(
@@ -24,25 +20,35 @@ def extract_features(image):
         flags = cv2.CASCADE_SCALE_IMAGE
     )
 
-    print "Found {0} faces!".format(len(faces))
-
-    # Draw a rectangle around the faces
+    # iterate over all identified faces and try to find eyes
     for (x, y, w, h) in faces:
         cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = image[y:y+h, x:x+w]
+
+        eyes = eye_cascade.detectMultiScale(roi_gray, minSize=(30, 30))
+        for (ex,ey,ew,eh) in eyes:
+            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(255,0,0),2)
+
+        #noses = nose_cascade.detectMultiScale(roi_gray, minSize=(100, 30))
+        #for (ex,ey,ew,eh) in noses:
+        #    cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,0,255),2)
 
 
     cv2.imshow('img', image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-def make_snapshot():
-    image = cv2.imread('poi_1.jpg')
-    return image
 
 if __name__ == "__main__":
-    if len(sys.argv) >= 2:
-        image = cv2.imread(sys.argv[1])
-    else:
-        image = make_snapshot()
 
+    if len(sys.argv) >= 2:
+        image_file = sys.argv[1]
+    else:
+        sleep(2)
+        image_file = 'snapshot.jpg'
+        picamera.PiCamera().capture(image_file)
+
+    image = cv2.imread(image_file)
     extract_features(image)
