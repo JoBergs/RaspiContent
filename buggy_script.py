e6515e66567bb495c0c8f1ae0916b4953b02ddf0
JoBergs <jo@knight-of-pi.org>
2017-04-07 13:25:55 +0200

    tutorial hint for buggy_script

--- buggy_script.py
+++ buggy_script.py
@@ -1,3 +1,7 @@
+# this is a buggy script for demonstration of the IPython Debugger IPDB.
+# Checkout the tutorial at
+# http://www.knight-of-pi.org/fix-your-bugs-using-the-ipyhton-debugger-ipdb-on-a-raspberry-pi/
+
 def calculate(a, b):
     return a / b
 
