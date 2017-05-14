ff45a47513ff36527dea701c638672b8de014877
JoBergs <jo@knight-of-pi.org>
2017-05-14 09:03:24 +0200

    GPIO input test Python version independent

--- GPIO_input_test.py
+++ GPIO_input_test.py
@@ -12,7 +12,7 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 def action(pin):
-    print 'Sensor detected action!'
+    print('Sensor detected action!')
     return
 
 GPIO.add_event_detect(7, GPIO.RISING)
@@ -20,7 +20,7 @@ GPIO.add_event_callback(7, action)
 
 try:
     while True:
-        print 'alive'
+        print('alive')
         time.sleep(0.5)
 except KeyboardInterrupt:
     GPIO.cleanup()
