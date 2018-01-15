8f3864bd2a893d7790aead8f5ecedc6688e26bd5
JoBergs <jo@knight-of-pi.org>
2018-01-15 15:59:46 +0100

    add browser test demo script

---
+++ browser_test.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from pyvirtualdisplay import Display
+
+display = Display(visible=0, size=(800, 600))
+display.start()
+
+profile = webdriver.FirefoxProfile()
+profile.native_events_enabled = False
+driver = webdriver.Firefox(profile)
+
+driver.get("http://www.google.com")
+print(driver.page_source.encode('utf-8'))
+driver.quit()
