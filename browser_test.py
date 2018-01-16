750e842d9839b60de3d8c13472669565281a91b5
JoBergs <jo@knight-of-pi.org>
2018-01-16 09:58:45 +0100

    update browser test script

--- browser_test.py
+++ browser_test.py
@@ -7,7 +7,23 @@ display.start()
 profile = webdriver.FirefoxProfile()
 profile.native_events_enabled = False
 driver = webdriver.Firefox(profile)
+driver.set_page_load_timeout(60)
 
 driver.get("http://www.duckduckgo.com")
-print(driver.page_source.encode('utf-8'))
+# print(driver.page_source.encode('utf-8'))
+
+# get the search textbox
+search_field = driver.find_element_by_id("search_form_input_homepage")
+search_field.clear()
+
+# enter search keyword and submit
+search_field.send_keys("Running a WordPress blog locally on a Raspberry Pi with WP-Duplicator")
+search_field.submit()
+
+result_list = driver.find_elements_by_class_name("result")
+results = [tmp.text for tmp in result_list]
+
+assert any(["knight-of-pi" in text for text in results]) == True
+assert not all(["knight-of-pi" in text for text in results]) == False
+
 driver.quit()
