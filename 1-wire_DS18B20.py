bf6bf643e09495fd5c69f88ed71c4960bff3a421
JoBergs <jo@knight-of-pi.org>
2018-01-17 14:01:40 +0100

    1-wire temp sensor script: log temperatures as numpy array

--- 1-wire_DS18B20.py
+++ 1-wire_DS18B20.py
@@ -1,36 +1,49 @@
 # WIP! DO NOT USE!
 
-import glob, os
+import glob, os, time
+import numpy as np
 
-# modify this:
-#   make a function that perpetually reads the temperature and stores it to a file
 
 class TemperatureSensor:
     def __init__(self, base_path= '/sys/bus/w1/devices/', 
                      file_name='/w1_slave', default=25.0):
 
         self.default = default
-        self.sensor_path = glob.glob(base_path + '28*')[0] + file_name 
 
-    def get_value(self):
+        try:
+            self.sensor_path = glob.glob(base_path + '28*')[0] + file_name 
+        except:
+            print("Bad sensor path")
+
+    def get_temperature(self):
         temperature = self.default
 
         try:           
             with open(self.sensor_path, 'r') as f:
-                line = f.readline()
-                crc = line.rsplit(' ',1)
-                
-                if 'YES\n' in crc:
-                    line = f.readline() # read 2nd line
-                    temperature = round(float(line.rsplit('t=',1)[1])/1000, 1)    
-                    print("Temperature is " + str(temperature) )
-                else:
-                    raise Exception()        
+                data = f.read()
+
+                if 'YES\n' not in data:
+                    raise Exception()
+                temperature = round(float(data.rsplit('t=',1)[1])/1000, 1)    
         except:          
             print("Can't read temperature sensor!")   
 
         return float(temperature) 
 
+    def log_temperature(self, interval=5.0):
+        temperatures = []
+        
+        while True:
+            time.sleep(interval)
+            temperatures.append(self.get_temperature())
+            print('Temperature: ' + str(temperatures[-1]))
+            np.save('temperatures', np.array(temperatures))
+            print(temperatures)
+            
+        # load stored temperatures with
+        # np.load('temperatures.npy')
+
 if __name__ == "__main__":
     sensor = TemperatureSensor()
-    print(sensor.get_value())
+    print(sensor.get_temperature())
+    # sensor.log_temperature()
