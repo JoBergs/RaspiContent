713d7ef9dc018be7b85553cca27686c83278416a
JoBergs <jo@knight-of-pi.org>
2018-06-10 17:42:25 +0200

    bump

--- thread_starter.py
+++ thread_starter.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 #encoding:utf-8
-#Tutorial: http://www.knight-of-pi.org/[FILL OUT YAY]
+#Tutorial: http://www.knight-of-pi.org/[FILL OUT]
 #Licence: http://creativecommons.org/licenses/by-nc-sa/3.0/
 # Author: Johannes Bergs
 
