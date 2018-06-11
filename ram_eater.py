5864dc68f432bee0349acf701e10703a0a97ddf8
JoBergs <jo@knight-of-pi.org>
2018-06-11 06:47:17 +0200

    ram_eater now takes depth as input

--- ram_eater.py
+++ ram_eater.py
@@ -4,11 +4,16 @@
 #Licence: http://creativecommons.org/licenses/by-nc-sa/3.0/
 # Author: Johannes Bergs
 
-from time import sleep
+import sys, time
 
-n = 100
-tmp = [[x for x in range(0, n)] for y in range(0, n) for z in range(0, n)]
+WAIT = 10
 
-sleep(10)
+def eat_RAM(depth=300):
+    tmp = [[x for x in range(0, depth)] for y in range(0, depth) for z in range(0, depth)]
+    time.sleep(WAIT)
 
 if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        eat_RAM(int(sys.argv[1]))
+    else:
+        eat_RAM()
