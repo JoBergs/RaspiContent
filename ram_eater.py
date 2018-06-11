d9571cd15cd281b6e827996596dec69abd4ed3f5
JoBergs <jo@knight-of-pi.org>
2018-06-11 06:39:31 +0200

    add ram_eater script for clusterHAT demo

---
+++ ram_eater.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+#encoding:utf-8
+#Tutorial: http://www.knight-of-pi.org/[FILL OUT]
+#Licence: http://creativecommons.org/licenses/by-nc-sa/3.0/
+# Author: Johannes Bergs
+
+from time import sleep
+
+n = 100
+tmp = [[x for x in range(0, n)] for y in range(0, n) for z in range(0, n)]
+
+sleep(10)
+
+if __name__ == "__main__":
