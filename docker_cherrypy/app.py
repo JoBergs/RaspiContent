c5366be110170c2e4c95cadbd27209874050d31a
JoBergs <jo@knight-of-pi.org>
2018-06-12 15:32:13 +0200

    Dockerfile for cherrypy: move to py3

--- docker_cherrypy/app.py
+++ docker_cherrypy/app.py
@@ -1,3 +1,9 @@
+#!/usr/bin/python3
+#encoding:utf-8
+#Tutorial: http://www.knight-of-pi.org/docker_and_the_raspberry_pi_cherrypy_in_a_container
+#Licence: http://creativecommons.org/licenses/by-nc-sa/3.0/
+# Author: Johannes Bergs
+
 import cherrypy
  
 class RPiServer(object):
