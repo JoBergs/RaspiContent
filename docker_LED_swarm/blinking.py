849955f0f284b54beeec18175373c735cb4e6885
JoBergs <jo@knight-of-pi.org>
2018-06-15 13:01:17 +0200

    add files for Docker swarm LED demo

---
+++ docker_LED_swarm/blinking.py
@@ -0,0 +1,22 @@
+import os
+import time
+
+import RPi.GPIO as GPIO
+
+# Set up the GPIO library to use Raspberry Pi
+# board pin numbers
+GPIO.setmode(GPIO.BOARD)
+# Set up the pin numbers we are using for each LED
+# pin number
+# LED_PIN=int(os.environ['LED_PIN'])
+LED_PIN = 11
+
+# Set Pin 11, 16 and 7 on the GPIO header to act as an output
+GPIO.setup(LED_PIN,GPIO.OUT)
+
+# This loop runs forever and handles the LED
+while True:
+    GPIO.output(LED_PIN,GPIO.HIGH)
+    time.sleep(1)
+    GPIO.output(LED_PIN,GPIO.LOW)
+    time.sleep(1)
