a4192dd13de90b47d39eaa725803df5692f4b96f
JoBergs <jo@knight-of-pi.org>
2018-06-19 08:09:30 +0200

    Dockerfile now installs RPi.GPIO

--- docker_LED_swarm/app.py
+++ docker_LED_swarm/app.py
@@ -1,24 +1,27 @@
-import os
+#!/usr/bin/python3
+#encoding:utf-8
+#Tutorial: http://www.knight-of-pi.org/docker_and_the_raspberry_pi_cherrypy_in_a_container
+#Licence: http://creativecommons.org/licenses/by-nc-sa/3.0/
+# Author: Johannes Bergs
+
 import time
 
 import RPi.GPIO as GPIO
 
-# Set up the GPIO library to use Raspberry Pi
-# board pin numbers
 GPIO.setmode(GPIO.BOARD)
-# Set up the pin numbers we are using for each LED
-# pin number
-# LED_PIN=int(os.environ['LED_PIN'])
 LED_PIN = 11
-
-# Environment variables are a way to pass distinct data to nodes for initialisation ect.
-
-# Set Pin 11, 16 and 7 on the GPIO header to act as an output
 GPIO.setup(LED_PIN,GPIO.OUT)
 
-# This loop runs forever and handles the LED
-while True:
+def blink():
     GPIO.output(LED_PIN,GPIO.HIGH)
-    time.sleep(1)
+    time.sleep(0.5)    
     GPIO.output(LED_PIN,GPIO.LOW)
-    time.sleep(1)
+    time.sleep(0.5)
+ 
+if __name__ == '__main__':
+    try:
+        while True:
+            blink()
+    except:
+        print("Bye!")
+        GPIO.cleanup()
