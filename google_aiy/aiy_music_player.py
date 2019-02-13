445aea76042eb8f6b9f242a0725d2c9a440bdb22
JoBergs <jo@knight-of-pi.org>
2019-02-13 15:46:43 +0000

    update aiy_music_player

--- google_aiy/aiy_music_player.py
+++ google_aiy/aiy_music_player.py
@@ -1,129 +1,83 @@
-#!/usr/bin/env python3
-# Copyright 2017 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+#!/usr/bin/python3
+#encoding:utf-8
+#Tutorial: http://www.knight-of-pi.org/a-voice-controlled-boombox-with-the-google-aiy-voice-hat/
+#Licence: http://creativecommons.org/licenses/by-nc-sa/3.0/
+# Author: Johannes Bergs
 
-# MODIFIED as youtube music player
-# Original file: /home/pi/AIY-voice-kit-python/src/examples/voice/assistant_library_with_local_commands_demo.py
-# Tutorial: http://www.knight-of-pi.org/measuring-and-logging-temperature-with-the-1-wire-sensor-ds18b20-and-a-raspberry-pi/
-# Licence: http://creativecommons.org/licenses/by-nc-sa/3.0/
-# Extended by Johannes Bergs
+import logging, os, platform, subprocess, sys, time
 
-
-"""Run a recognizer using the Google Assistant Library.
-
-The Google Assistant Library has direct access to the audio API, so this Python
-code doesn't need to record audio. Hot word detection "OK, Google" is supported.
-
-It is available for Raspberry Pi 2/3 only; Pi Zero is not supported.
-"""
-
-import logging
-import platform
-import subprocess
-import sys
-import time
-
-import aiy.assistant.auth_helpers
-from aiy.assistant.library import Assistant
-import aiy.audio
-import aiy.voicehat
 from google.assistant.library.event import EventType
 
-logging.basicConfig(
-    level=logging.INFO,
-    format="[%(asctime)s] %(levelname)s:%(name)s:%(message)s"
-)
+from aiy.assistant import auth_helpers
+from aiy.assistant.library import Assistant
+from aiy.board import Board, Led
+import aiy.voice.tts
 
 
 def power_off_pi():
-    aiy.audio.say('Good bye!')
+    aiy.voice.tts.say('Good bye!')
     subprocess.call('sudo shutdown now', shell=True)
 
 
 def reboot_pi():
-    aiy.audio.say('See you in a bit!')
+    aiy.voice.tts.say('See you in a bit!')
     subprocess.call('sudo reboot', shell=True)
-
-
-def say_ip():
-    ip_address = subprocess.check_output("hostname -I | cut -d' ' -f1", shell=True)
-    aiy.audio.say('My IP address is %s' % ip_address.decode('utf-8'))
 
 
 playshell = None
 
-def play(say, keyword):
+def play(say):
         track = say.split('play')[-1]
 
         global playshell
         if (playshell == None):
-            playshell = subprocess.Popen(["/usr/local/bin/mpsyt",""],stdin=subprocess.PIPE,stdout=subprocess.PIPE)
+            playshell = subprocess.Popen(["/usr/local/bin/mpsyt",""], stdin=subprocess.PIPE)
 
         playshell.stdin.write(bytes('/' + track + '\n1\n', 'utf-8'))
         playshell.stdin.flush()
 
-def process_event(assistant, event):
-    status_ui = aiy.voicehat.get_status_ui()
+
+def process_event(led, event):
+    logging.info(event)
+
     if event.type == EventType.ON_START_FINISHED:
-        status_ui.status('ready')
-        aiy.audio.say("Hello")
-        if sys.stdout.isatty():
-            print('Say "OK, Google" then speak, or press Ctrl+C to quit...')
+        led.state = Led.BEACON_DARK  # Ready.
+        logging.info('Say "OK, Google" then speak, or press Ctrl+C to quit...')
 
     elif event.type == EventType.ON_CONVERSATION_TURN_STARTED:
-        status_ui.status('listening')
+        led.state = Led.ON  # Listening.
+
+    elif event.type == EventType.ON_END_OF_UTTERANCE:
+        led.state = Led.PULSE_QUICK  # Thinking.
+
+    elif (event.type == EventType.ON_CONVERSATION_TURN_FINISHED
+          or event.type == EventType.ON_CONVERSATION_TURN_TIMEOUT
+          or event.type == EventType.ON_NO_RESPONSE):
+        led.state = Led.BEACON_DARK
+
+    elif event.type == EventType.ON_ASSISTANT_ERROR and event.args and event.args['is_fatal']:
+        sys.exit(1)
 
     elif event.type == EventType.ON_RECOGNIZING_SPEECH_FINISHED and event.args:
         print('You said:', event.args['text'])
         text = event.args['text'].lower()
         if text == 'power off':
-            assistant.stop_conversation()
             power_off_pi()
         elif text == 'reboot':
-            assistant.stop_conversation()
             reboot_pi()
-        elif text == 'ip address':
-            assistant.stop_conversation()
-            say_ip()
         elif 'play' in text:
-            assistant.stop_conversation()
-            play(text, 'play')
+            play(text)
         elif 'stop' in text:
-            assistant.stop_conversation()
             pkill = subprocess.Popen(["/usr/bin/pkill","vlc"],stdin=subprocess.PIPE)
-
-    elif event.type == EventType.ON_END_OF_UTTERANCE:
-        status_ui.status('thinking')
-
-    elif (event.type == EventType.ON_CONVERSATION_TURN_FINISHED
-          or event.type == EventType.ON_CONVERSATION_TURN_TIMEOUT
-          or event.type == EventType.ON_NO_RESPONSE):
-        status_ui.status('ready')
-
-    elif event.type == EventType.ON_ASSISTANT_ERROR and event.args and event.args['is_fatal']:
-        sys.exit(1)
 
 
 def main():
-    if platform.machine() == 'armv6l':
-        print('Cannot run hotword demo on Pi Zero!')
-        exit(-1)
+    logging.basicConfig(level=logging.INFO)
 
-    credentials = aiy.assistant.auth_helpers.get_assistant_credentials()
-    with Assistant(credentials) as assistant:
+    credentials = auth_helpers.get_assistant_credentials()
+    with Board() as board, Assistant(credentials) as assistant:
         for event in assistant.start():
-            process_event(assistant, event)
+            process_event(board.led, event)
 
 
 if __name__ == '__main__':
